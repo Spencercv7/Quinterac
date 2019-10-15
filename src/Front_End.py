e4a1a078da80d2769c2518828eb05b470beba31b
spenc <16scpv@queensu.ca>
2019-10-15 19:24:54 -0400

    Initilization Bash Script
    Frontend initilization with bash script.

---
+++ src/Front_End.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def frontend(accountList, transactionSummary):
+    print(accountList, transactionSummary)
+
+
+perm1 = sys.argv[1]
+perm2 = sys.argv[2]
+frontend(perm1, perm2)
---
+++ src/frontend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+#
+python3 Front_End.py $1 $2
