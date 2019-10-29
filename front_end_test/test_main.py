17ec14979d320aeec3c882f722d6d1d3d54ff60e
benlammers <21benlammers@gmail.com>
2019-10-29 15:14:02 -0400

    Removed Redundencies, Added Workflows
    Removed redundencies in test files and added Github workflow folder

--- front_end_test/test_main.py
+++ front_end_test/test_main.py
@@ -8,34 +8,6 @@ import front_end.run as app
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-def test_r1(capsys):
-    """Testing r2. Self-contained (i.e. everything in the code approach)
-    [my favorite - all in one place with the code]
-
-    Arguments:
-        capsys -- object created by pytest to capture stdout and stderr
-    """
-    helper(
-        capsys=capsys,
-        terminal_input=[
-            'login',
-            'machine',
-            'logout',
-            'exit'
-        ],
-        intput_valid_accounts=[
-            '1000327',
-            '0000000'
-        ],
-        expected_tail_of_terminal_output=[
-            'Exiting System'
-        ],
-        expected_output_transactions=[
-            'EOS'
-        ]
-    )
-
-
 def helper(
         capsys,
         terminal_input,
